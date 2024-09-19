10884949d8d739622b00e1f3d671f7b75507cdd4
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-19 13:58:23 -0500

    l

--- Player.cs
+++ Player.cs
@@ -8,5 +8,32 @@ namespace Queues_Jackson_Lydia
 {
     internal class Player
     {
+        public int Health {  get; set; }
+
+        public string GamerTag { get; }
+
+        public Player(int healthParam, string nameParam)
+        {
+            Health = healthParam;
+            GamerTag = nameParam;
+        }
+
+        public void JoinGame(Game game, Queue<Player> queue)
+        {
+            if(game.CurrentPlayers == game.MaxPlayers)
+            {
+                queue.Enqueue(this);
+            }
+            else
+            {
+                Console.WriteLine("Joining Game.");
+                game.CurrentPlayers++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return GamerTag;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Queues_Jackson_Lydia
+﻿using System;
+using System.Collections.Generic;
+namespace Queues_Jackson_Lydia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game myGame = new Game(50, 49);
+
+            Queue<Player> waitingToJoinQ = new Queue<Player>();
+
+            Player p1 = new Player(100, "nicofreak0");
+            Player p2 = new Player(100, "Nightstorm64");
+            Player p3 = new Player(100, "SweetCottonTail");
+            Player p4 = new Player(100, "BeetleJuiceBoi");
+
+            p1.JoinGame(myGame, waitingToJoinQ);
+            p2.JoinGame(myGame, waitingToJoinQ);
+            p3.JoinGame(myGame, waitingToJoinQ);
+            p4.JoinGame(myGame, waitingToJoinQ);
+
+            Console.WriteLine("Players in the Queue:");
+            foreach(Player player in waitingToJoinQ) 
+            {
+                Console.WriteLine(player);
+            }
+
+            myGame.KickPlayer();
+            myGame.CheckQueue(waitingToJoinQ);
+
+
+
+            Console.WriteLine("\nPlayers in the Queue:");
+            foreach (Player player in waitingToJoinQ)
+            {
+                Console.WriteLine(player);
+            }
         }
     }
 }
